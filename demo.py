187fb193e8f0b17baf9393dafd3b6ed91fc6613c
imran <mohammedimranat@gmail.com>
2026-02-09 18:04:39 +0530

    Add initial variables and print statements to demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
 print("Hello world")
+initial='A'
+first_name = "Mohammed"
+Last_name = '''imran'''
+age=19
+persentage = 90.9
+male = True
+print(age)
+print(type(initial), type(first_name), type(Last_name), type(age), type(persentage), type(male))
+
+# Practice set 1 
+a = 2
+b = 3
+print(a+b)
+# 
+
