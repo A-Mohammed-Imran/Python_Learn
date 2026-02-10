446788d01ef125bf8670baf98c73a0ee2920b008
imran <mohammedimranat@gmail.com>
2026-02-10 16:03:57 +0530

    Refactor demo.py to include sum calculation and improve comments

--- demo.py
+++ demo.py
@@ -11,6 +11,20 @@ print(type(initial), type(first_name), type(Last_name), type(age), type(persenta
 # Practice set 1 
 a = 2
 b = 3
+sum = a + b
 print(a+b)
-# 
+print(sum)
 
+# Comment
+
+# This is a single line comment
+
+"""
+This is a multi-line comment
+or docstring
+"""
+
+'''
+This is a multi-line comment
+or docstring
+''' 
