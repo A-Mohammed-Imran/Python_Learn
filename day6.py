9ff7632f295a7771f59fb8e0dc1f2ac8fd7ee396
imran <mohammedimranat@gmail.com>
2026-02-11 17:47:18 +0530

    Add day6.py to demonstrate variable types and data type printing

---
+++ day6.py
@@ -0,0 +1,17 @@
+# variables
+# that can hold different types of data
+
+a = 123456 
+b = "Hello world"
+c = True
+d = None
+
+print(a, b, c, d)
+
+# Data types in python
+
+a = 123456 # integer data type
+b = "Hello world" # string data type
+c = True # boolean data type   
+d = None # None data type
+print(type(a), type(b), type(c), type(d))
