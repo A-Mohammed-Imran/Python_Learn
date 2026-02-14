8b3ba3ad7621458bf88d6fb6db5e01885b515380
imran <mohammedimranat@gmail.com>
2026-02-14 15:53:57 +0530

    Refactor calculator implementation: move to day7andday8reow_of_task.py and remove day7.py

---
+++ day7andday8reow_of_task.py
@@ -0,0 +1,24 @@
+# Calculator using python
+
+# Exercise 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+add = (a + b)
+sub = (a - b)
+mul = (a * b)
+expo = (a ** b)
+div = (a / b)
+mod = (a % b)
+flodev = ( a // b )
+
+
+print(f"Addition of {a} and {b} is : {add}")
+print(f"Subraction of {a} and {b} is : {sub}")
+print(f"Multiplication of {a} and {b} is : {mul}")
+print(f"Exponation of {a} and {b} is : {expo}")
+print(f"Deviation of {a} and {b} is : {div}")
+print(f"Modulation of {a} and {b} is : {mod}")
+print(f"Float devition of {a} and {b} is : {flodev}")
+
---
+++ day9.py
@@ -0,0 +1,9 @@
+# Type casting
+
+# it is used to convert one data type to another data type
+# and also it is a function not function
+
+a = 1234
+b = "1234"
+print( a + int(b))
+
