8fe4e9be2742410a96d827c2f59aac2bc617e7da
imran <mohammedimranat@gmail.com>
2026-02-17 17:44:22 +0530

    Enhance day9.py with additional comments on type casting concepts

--- day9.py
+++ day9.py
@@ -3,7 +3,18 @@
 # it is used to convert one data type to another data type
 # and also it is a function not function
 
+# there are two types
+
+# 1 Implicit type casting
+
 a = 1234
 b = "1234"
 print( a + int(b))
+# when we convert data type manually , it's consider as Implicity conversion
 
+# 2 Explicit type casting
+
+a = 1.234
+b = 4
+print(a + b)
+# it converts data type automatically by python interpreter , it's consiter as Explicity conversion
