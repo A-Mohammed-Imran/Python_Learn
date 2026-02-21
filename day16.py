9fda8e2c072d23807342f96b156421356662fe69
imran <mohammedimranat@gmail.com>
2026-02-21 14:01:29 +0530

    Add match statement implementation in day16.py and create day17.py with iterative statement examples

--- day16.py
+++ day16.py
@@ -1,2 +1,15 @@
 # Match Statement
 
+x = int(input("Enter a 1 to 4 number:"))
+
+# match method is used to check the multiple conditions in a more elegant way than if-elif-else statement it's similar to switch case statement in other programming languages
+
+match x:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("three")
+    case _:
+        print("Invalid number")
---
+++ day17.py
@@ -0,0 +1,8 @@
+# Itreative Statement 
+
+# for loop
+names = ["imran", "azhan", "sufiyan"]
+for name in names:
+    print(name)
+    for i in name:
+        print(i)
