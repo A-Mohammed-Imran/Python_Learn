295e1e7b0fa2684dd8d8cfc48c4ad4c691e94886
imran <mohammedimranat@gmail.com>
2026-02-22 22:14:34 +0530

    Enhance comments in day17.py to clarify variable scope in for loops

--- day17.py
+++ day17.py
@@ -4,5 +4,7 @@
 names = ["imran", "azhan", "sufiyan"]
 for name in names:
     print(name)
+    # the new variable name is only for the loop and it will not change the original variable names
     for i in name:
         print(i)
+        # Again the new variable i is only for the loop and it will not change the original variable name
