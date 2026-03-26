4e92125df125e41556149eb84ffb361684df921f
imran <mohammedimranat@gmail.com>
2026-03-26 18:24:48 +0530

    Add formatted string examples in day28.py and implement a square function with docstring in day29.py

--- day28.py
+++ day28.py
@@ -6,3 +6,5 @@ print(f" My name is 'A Mohammed {a}'")
 
 po = 23.454545
 print(f"For only {po:.3f} dollers today !!")
+
+print(f" The sum of 2 and 5 is {2 + 5}")
---
+++ day29.py
@@ -0,0 +1,7 @@
+# Doc Strings
+
+def sqr(n):
+    '''Returns the square of a number'''
+    print(n**2)
+sqr(5)
+print(sqr.__doc__)
