8ab259e41b70b51de274373b7600d6204bed1b44
imran <mohammedimranat@gmail.com>
2026-03-30 17:52:41 +0530

    Add day35.py with an example of for loop using else statement and add day36.py for multiplication table generation

---
+++ day35.py
@@ -0,0 +1,8 @@
+# For loop with else statement
+
+for i in range(5):
+    print(i)
+else:
+    print("Loop completed successfully without break.")
+
+# the else block will execute after the for loop finishes iterating over all items, unless a break statement is encountered.
